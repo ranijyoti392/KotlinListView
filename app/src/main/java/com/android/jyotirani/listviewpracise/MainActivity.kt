e41f08ea0db27aa80dfffee0278adb0569c88d47
Jyoti Rani <ranijyoti392@gmail.com>
2018-02-22 18:31:20 +0530

    Added Convert View.

--- app/src/main/java/com/android/jyotirani/listviewpracise/MainActivity.kt
+++ app/src/main/java/com/android/jyotirani/listviewpracise/MainActivity.kt
@@ -3,6 +3,7 @@ package com.android.jyotirani.listviewpracise
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -28,7 +29,10 @@ class MainActivity : AppCompatActivity() {
         private val mContext: Context
 
         private val names = arrayListOf<String>(
-                "Donald Trump", "Steve Jobs", "Tim Cook"
+                "Donald Trump", "Steve Jobs", "Tim Cook", "Mark Zukesberg",
+                "Donald Trump", "Steve Jobs", "Tim Cook", "Mark Zukesberg",
+                "Donald Trump", "Steve Jobs", "Tim Cook", "Mark Zukesberg",
+                "Donald Trump", "Steve Jobs", "Tim Cook", "Mark Zukesberg"
         )
 
         init {
@@ -36,20 +40,32 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-            val layoutInflater = LayoutInflater.from(mContext)
-            val rowMain = layoutInflater.inflate(R.layout.row_main, parent, false)
 
-            val positionTextView = rowMain.findViewById<TextView>(R.id.position_textview)
-            positionTextView.text = "Row Number: $position"
+            val rowMain: View
 
-            val nameTextView = rowMain.findViewById<TextView>(R.id.name_textView)
-            nameTextView.text = names.get(position)
+            //checking if convertview is null, meaning we have to inflate a new row
+            if(convertView == null) {
+                val layoutInflater = LayoutInflater.from(mContext)
+                rowMain = layoutInflater.inflate(R.layout.row_main, parent, false)
+
+                val positionTextView = rowMain.findViewById<TextView>(R.id.position_textview)
+                val nameTextView = rowMain.findViewById<TextView>(R.id.name_textView)
+                val viewHolder = ViewHolder(nameTextView, positionTextView)
+
+                rowMain.tag = viewHolder
+
+            } else {
+                //we have our rows as converView, so set rowMain as that view
+                rowMain = convertView
+            }
+
+          //  Log.v("getView", "findViewByID which is expensive")
+            val viewHolder = rowMain.tag as ViewHolder
+            viewHolder.nameTextView.text = names.get(position)
+            viewHolder.positionTextView.text = "Row Number: $position"
+            // nameTextView.text = names.get(position)
 
             return rowMain
-
-//            val textView = TextView(mContext)
-//            textView.setText("Here is my row for my listview.")
-//            return textView
         }
 
         override fun getItem(position: Int): Any {
@@ -65,5 +81,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
+    private class ViewHolder(val nameTextView: TextView, val positionTextView: TextView)
 }
